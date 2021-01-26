54690943985591f86a0393bf7e2acc19a795e4bf
dongbohu <dongbo.hu@gmail.com>
2021-01-26 08:31:10 -0500

    test

--- recover_submitted.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Put submitted file back to "submitted/" dir
-git mv processed/PR-76/gtex_v7_ciseqtl_brain_hypothalamus.yaml submitted/
-git ci -m "clean"
-git push
-
-# Remove intermediate file
-rm -f processed/s3_list.txt
